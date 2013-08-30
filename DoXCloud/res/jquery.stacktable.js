91cddb54b403e124da630d173d45ddcae4974c47
Ollie Terrance <ollie.terrance@live.co.uk>
2013-08-30 10:12:09 +0100

    Rewrite stacktable plugin

--- DoXCloud/res/jquery.stacktable.js
+++ DoXCloud/res/jquery.stacktable.js
@@ -8,50 +8,46 @@
  *
  * jQuery plugin for stacking tables on small screens
  *
- * This version has been modified for a custom table
- * layout (one table per row of original table).
+ * This version has been modified for a custom layout
+ * (one table per row of original table), and to clone
+ * the original cells in order to retain functionality.
  *
  */
 
-;(function($) {
-
-  $.fn.stacktable = function(options) {
-    var $tables = this,
-        defaults = {id:'stacktable',hideOriginal:false},
-        settings = $.extend({}, defaults, options),
-        stacktable;
-
-    return $tables.each(function() {
-      $table = $(this);
-      $topRow = $table.find('tr').eq(0);
-      $table.find('tr').each(function(index,value) {
-        if (index>0) {
-          var $stacktable = $('<table class="'+settings.id+'"><tbody></tbody></table>');
-          if (typeof settings.myClass !== undefined) $stacktable.addClass(settings.myClass);
-          var markup = '';
-          markup += '<tr>';
-          $(this).find('td').each(function(index,value) {
-            if (index===0) {
-              markup += '<tr><th class="st-head-row" colspan="2">'+$(this).html()+'</th></tr>';
-            } else {
-              if ($(this).html() !== ''){
-                markup += '<tr>';
-                if ($topRow.find('td,th').eq(index).html()){
-                  markup += '<td class="st-key">'+$topRow.find('td,th').eq(index).html()+'</td>';
-                } else {
-                  markup += '<td class="st-key"></td>';
+(function($) {
+    $.fn.stacktable = function(options) {
+        var $tables = this;
+        var defaults = {
+            id: "stacktable",
+            hideOriginal: false
+        };
+        var settings = $.extend({}, defaults, options);
+        var stacktable;
+        return $tables.each(function() {
+            $table = $(this);
+            $topRow = $table.find("tr").eq(0);
+            $table.find("tr").each(function(index, value) {
+                if (index > 0) {
+                    var $stacktable = $("<table class='" + settings.id + "'><tbody></tbody></table>");
+                    if (typeof settings.myClass !== undefined) {
+                        $stacktable.addClass(settings.myClass);
+                    }
+                    $(this).find("td").each(function(index, value) {
+                        var row = $("<tr/>");
+                        if (index === 0) {
+                            row.append($(this).clone(true).addClass("st-head-row").attr("colspan", 2));
+                        } else if ($(this).html() !== "") {
+                            row.append($topRow.find("td, th").eq(index).clone(true).addClass("st-key"));
+                            row.append($(this).clone(true).addClass("st-val"));
+                        }
+                        $stacktable.append(row);
+                    });
+                    $table.before($stacktable);
                 }
-                markup += '<td class="st-val">'+$(this).html()+'</td>';
-                markup += '</tr>';
-              }
+            });
+            if (settings.hideOriginal) {
+                $table.hide();
             }
-          });
-          $stacktable.append($(markup));
-          $table.before($stacktable);
-        }
-      });
-      if (settings.hideOriginal) $table.hide();
-    });
-  };
-
-}(jQuery));
+        });
+    };
+})(jQuery);
